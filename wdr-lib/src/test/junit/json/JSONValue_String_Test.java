1ebd5a9c2599d51f070d8167c4f852a5a8ffa21c
Wendy Roome <wendy@wdroome.com>
2015-06-02 14:24:40 -0400

    For JSON Strings, do not automatically escape /.

--- wdr-lib/src/test/junit/json/JSONValue_String_Test.java
+++ wdr-lib/src/test/junit/json/JSONValue_String_Test.java
@@ -18,8 +18,10 @@ public class JSONValue_String_Test
 		check("simple", "hello there", "\"hello there\"");
 		check("esc1", "hello \"there\"", "\"hello \\\"there\\\"\"");
 		check("esc1b", "hello 'there'", "\"hello 'there'\"");
-		check("esc2", "\t \b \n \r \f \\ /", "\"\\t \\b \\n \\r \\f \\\\ \\/\"");
+		check("esc2", "\t \b \n \r \f \\ /", "\"\\t \\b \\n \\r \\f \\\\ /\"");
 		check("esc3", "\u1234 \u8765 \u9abc \u0fed", "\"\\u1234 \\u8765 \\u9abc \\u0fed\"");
+		check2("rev1", "\"\\/\"", "/");
+		check2("rev2", "\"a\\/b\"", "a/b");
 	}
 	
 	private void check(String desc, String src, String expectedJson)
@@ -34,5 +36,15 @@ public class JSONValue_String_Test
 			fail(desc + "/PARSE: Not JSONValue_String");
 		}
 		assertEquals(desc + "/PARSE", src, ((JSONValue_String)jvalue2).m_value);
+	}
+	
+	private void check2(String desc, String json, String expectedStr)
+				throws JSONParseException
+	{
+		JSONValue jvalue = new JSONParser().parse(new JSONLexan(json));
+		if (!(jvalue instanceof JSONValue_String)) {
+			fail(desc + "/PARSE: Not JSONValue_String");
+		}
+		assertEquals(desc, expectedStr, ((JSONValue_String)jvalue).m_value);
 	}
 }
