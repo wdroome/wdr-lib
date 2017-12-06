d5818d1df7bf603709b6d743fc8dc5ab81301f27
Wendy Roome <wendy@wdroome.com>
2017-12-06 08:49:02 -0500

    Add json StringArray, ObjectArray & DoubleArray classes.

---
+++ wdr-lib/src/com/wdroome/json/JSONValue_ObjectArray.java
@@ -0,0 +1,173 @@
+package com.wdroome.json;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/**
+ * A JSON array which is limited to Object values.
+ * @author wdr
+ */
+public class JSONValue_ObjectArray implements JSONValue, Iterable<JSONValue_Object>
+{
+	private final JSONValue_Array m_array;
+
+	/**
+	 * Create a new empty array.
+	 */
+	public JSONValue_ObjectArray()
+	{
+		m_array = new JSONValue_Array();
+	}
+
+	/**
+	 * Create a new String array from an existing JSON array.
+	 * Deletes all non-object elements from the array.
+	 * @param array The existing JSON array.
+	 */
+	public JSONValue_ObjectArray(JSONValue_Array array)
+	{
+		m_array = array;
+		for (ListIterator<JSONValue> iter = array.listIterator(); iter.hasNext(); ) {
+			JSONValue v = iter.next();
+			if (!(v instanceof JSONValue_Object)) {
+				iter.remove();
+			}
+		}
+	}
+	
+	/**
+	 * Return the underlying JSON array.
+	 * @return The underlying JSON array.
+	 */
+	public JSONValue_Array getJSONArray()
+	{
+		return m_array;
+	}
+	
+	/**
+	 * Return an element.
+	 * @param index The element's index.
+	 * @return The object value of the element.
+	 * 		Return null if it's not a JSON object.
+	 * @throws IndexOutOfBoundsException If index is invalid.
+	 */
+	public JSONValue_Object get(int index)
+	{
+		JSONValue value = m_array.get(index);
+		return (value instanceof JSONValue_Object) ? (JSONValue_Object)value : null;
+	}
+	
+	/**
+	 * Add a object value to the array.
+	 * @param value The value.
+	 */
+	public void add(JSONValue_Object value)
+	{
+		m_array.add(value);
+	}
+	
+	/**
+	 * Replace an array element.
+	 * @param index The element's index.
+	 * @param value The value.
+	 */
+	public JSONValue_Object set(int index, JSONValue_Object value)
+	{
+		JSONValue_Object prev = get(index);
+		m_array.set(index, value);
+		return prev;
+	}
+	
+	/**
+	 * Remove an array element.
+	 * @param index The element's index.
+	 * @return The previous value.
+	 * @throws IndexOutOfBoundsException If index is invalid.
+	 */
+	public JSONValue_Object remove(int index)
+	{
+		JSONValue_Object prev = get(index);
+		m_array.remove(index);
+		return prev;
+	}
+	
+	/**
+	 * Return the size of the array.
+	 * @return The size of the array.
+	 */
+	public int size()
+	{
+		return m_array.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Iterable#iterator()
+	 */
+	@Override
+	public Iterator<JSONValue_Object> iterator()
+	{
+		return new MyIterator();
+	}
+	
+	private class MyIterator implements Iterator<JSONValue_Object>
+	{
+		private final Iterator<JSONValue> m_arrayIter;
+		
+		private MyIterator()
+		{
+			m_arrayIter = m_array.iterator();
+		}
+		
+		public JSONValue_Object next()
+		{
+			JSONValue value = m_arrayIter.next();
+			return (value instanceof JSONValue_Object) ? (JSONValue_Object)value : null;		
+		}
+
+		/* (non-Javadoc)
+		 * @see java.util.Iterator#hasNext()
+		 */
+		@Override
+		public boolean hasNext()
+		{
+			return m_arrayIter.hasNext();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see JSONValue#isSimple()
+	 */
+	@Override
+	public boolean isSimple()
+	{
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see JSONValue#jsonType()
+	 */
+	@Override
+	public String jsonType()
+	{
+		return "ObjectArray";
+	}
+
+	/* (non-Javadoc)
+	 * @see JSONValue#writeJSON(JSONWriter)
+	 */
+	@Override
+	public void writeJSON(JSONWriter writer) throws IOException
+	{
+		m_array.writeJSON(writer);
+	}
+
+	/**
+	 * Return a compact JSON encoding of this array.
+	 */
+	@Override
+	public String toString()
+	{
+		return JSONUtil.toJSONString(this, false);
+	}
+}
--- wdr-lib/src/test/junit/json/JSONValue_Array_Test.java
+++ wdr-lib/src/test/junit/json/JSONValue_Array_Test.java
@@ -74,4 +74,101 @@ public class JSONValue_Array_Test
 			assertEquals("[" + i + "]", pval + i, jarr2.getString(i));
 		}
 	}
+	
+	@Test
+	public void test3() throws JSONParseException, JSONValueTypeException, JSONFieldMissingException
+	{
+		JSONValue_Array jarr = new JSONValue_Array();
+		jarr.add("hi");
+		jarr.add(1);
+		jarr.add(new JSONValue_BigInt(3));
+		jarr.add(new JSONValue_Object());
+		jarr.add("last");
+		
+		JSONValue_StringArray sarr1 = new JSONValue_StringArray(jarr, true);
+		sarr1.add("end");
+		assertEquals("size/cvt", 5, sarr1.size());
+		assertEquals("get(0)/cvt", "hi", sarr1.get(0));
+		assertEquals("get(1)/cvt", "1.0", sarr1.get(1));
+		assertEquals("get(2)/cvt", "3", sarr1.get(2));
+		assertEquals("get(3)/cvt", "last", sarr1.get(3));
+		assertEquals("get(4)/cvt", "end", sarr1.get(4));
+		assertEquals("eq1/cvt", "[\"hi\",\"1.0\",\"3\",\"last\",\"end\"]", sarr1.toString());
+		sarr1.set(0, "hello");
+		assertEquals("eq2/cvt", "[\"hello\",\"1.0\",\"3\",\"last\",\"end\"]", sarr1.toString());
+		sarr1.remove(1);
+		assertEquals("eq3/cvt", "[\"hello\",\"3\",\"last\",\"end\"]", sarr1.toString());
+	}
+	
+	@Test
+	public void test4() throws JSONParseException, JSONValueTypeException, JSONFieldMissingException
+	{
+		JSONValue_Array jarr = new JSONValue_Array();
+		jarr.add("hi");
+		jarr.add(1);
+		jarr.add(new JSONValue_BigInt(3));
+		jarr.add(new JSONValue_Object());
+		jarr.add("last");
+		
+		JSONValue_StringArray sarr1 = new JSONValue_StringArray(jarr, false);
+		sarr1.add("end");
+		System.out.println("XXX: " + sarr1.toString());
+		assertEquals("size/no-cvt", 3, sarr1.size());
+		assertEquals("get(0)/no-cvt", "hi", sarr1.get(0));
+		assertEquals("get(1)/no-cvt", "last", sarr1.get(1));
+		assertEquals("get(2)/no-cvt", "end", sarr1.get(2));
+		assertEquals("eq1/no-cvt", "[\"hi\",\"last\",\"end\"]", sarr1.toString());
+		sarr1.set(0, "hello");
+		assertEquals("eq2/no-cvt", "[\"hello\",\"last\",\"end\"]", sarr1.toString());
+		sarr1.remove(1);
+		assertEquals("eq3/no-cvt", "[\"hello\",\"end\"]", sarr1.toString());
+	}
+	
+	@Test
+	public void test5() throws JSONParseException, JSONValueTypeException, JSONFieldMissingException
+	{
+		JSONValue_Array jarr = new JSONValue_Array();
+		jarr.add("0");
+		jarr.add("0x");
+		jarr.add(1);
+		jarr.add(new JSONValue_BigInt(3));
+		jarr.add(new JSONValue_Object());
+		jarr.add("last");
+		
+		JSONValue_DoubleArray darr1 = new JSONValue_DoubleArray(jarr, true);
+		darr1.add(42);
+		assertEquals("size/cvt", 4, darr1.size());
+		assertEquals("get(0)/cvt", 0, darr1.get(0), .001);
+		assertEquals("get(1)/cvt", 1, darr1.get(1), .001);
+		assertEquals("get(2)/cvt", 3, darr1.get(2), .001);
+		assertEquals("get(3)/cvt", 42, darr1.get(3), .001);
+		assertEquals("eq1/cvt", "[0,1,3,42]", darr1.toString());
+		darr1.set(1, 1.5);
+		assertEquals("eq2/cvt", "[0,1.5,3,42]", darr1.toString());
+		darr1.remove(1);
+		assertEquals("eq3/cvt", "[0,3,42]", darr1.toString());
+	}
+	
+	@Test
+	public void tes6() throws JSONParseException, JSONValueTypeException, JSONFieldMissingException
+	{
+		JSONValue_Array jarr = new JSONValue_Array();
+		jarr.add("0");
+		jarr.add("0x");
+		jarr.add(1);
+		jarr.add(new JSONValue_BigInt(3));
+		jarr.add(new JSONValue_Object());
+		jarr.add("last");
+		
+		JSONValue_DoubleArray darr1 = new JSONValue_DoubleArray(jarr, false);
+		darr1.add(42);
+		assertEquals("size", 2, darr1.size());
+		assertEquals("get(0)", 1, darr1.get(0), .001);
+		assertEquals("get(1)", 42, darr1.get(1), .001);
+		assertEquals("eq1", "[1,42]", darr1.toString());
+		darr1.set(1, 1.5);
+		assertEquals("eq2", "[1,1.5]", darr1.toString());
+		darr1.remove(1);
+		assertEquals("eq3", "[1]", darr1.toString());
+	}
 }
