667497e120fa63c9969bb0bacad7cf9c363970c3
Wendy Roome <wendy@wdroome.com>
2022-05-07 15:42:53 -0400

    Add more to EOSCueInfo.

--- wdr-lib/src/main/java/com/wdroome/osc/eos/EOSCueInfo.java
+++ wdr-lib/src/main/java/com/wdroome/osc/eos/EOSCueInfo.java
@@ -44,12 +44,51 @@ public class EOSCueInfo implements Comparable<EOSCueInfo>
 	public static final int GET_CUE_INFO_REPLY_FLD_COLOR_DELAY = 10;
 	public static final int GET_CUE_INFO_REPLY_FLD_BEAM_TIME = 11;
 	public static final int GET_CUE_INFO_REPLY_FLD_BEAM_DELAY = 12;
+	public static final int GET_CUE_INFO_REPLY_FLD_RATE = 15;
+	public static final int GET_CUE_INFO_REPLY_FLD_MARK = 16;
+	public static final int GET_CUE_INFO_REPLY_FLD_BLOCK = 17;;
+	public static final int GET_CUE_INFO_REPLY_FLD_ASSERT = 18;
+	public static final int GET_CUE_INFO_REPLY_FLD_LINK = 19;
+	public static final int GET_CUE_INFO_REPLY_FLD_FOLLOW_TIME = 20;
+	public static final int GET_CUE_INFO_REPLY_FLD_HANG_TIME = 21;
+	public static final int GET_CUE_INFO_REPLY_FLD_ALL_FADE = 22;
+	public static final int GET_CUE_INFO_REPLY_FLD_LOOP = 23;
+	public static final int GET_CUE_INFO_REPLY_FLD_PART_COUNT = 26;
+	public static final int GET_CUE_INFO_REPLY_FLD_NOTES = 27;
+	public static final int GET_CUE_INFO_REPLY_FLD_SCENE = 28;
+	public static final int GET_CUE_INFO_REPLY_FLD_SCENE_END = 29;
+	public static final int GET_CUE_INFO_REPLY_FLD_PART_INDEX = 30;
+
 	
 	private int m_cuelist;
 	private int m_cueIndex = -1;
 	private EOSCueNumber m_cueNumber = null;
 	private String m_uuid = "";
 	private String m_label = "";
+	private int m_upTimeMS = -1;
+	private int m_upDelayMS = -1;
+	private int m_downTimeMS = -1;
+	private int m_downDelayMS = -1;
+	private int m_focusTimeMS = -1;
+	private int m_focusDelayMS = -1;
+	private int m_colorTimeMS = -1;
+	private int m_colorDelayMS = -1;
+	private int m_beamTimeMS = -1;
+	private int m_beamDelayMS = -1;
+	private int m_rate = -1;
+	private String m_mark = "";
+	private String m_block = "";
+	private String m_assert = "";
+	private String m_link = "";
+	private int m_followTimeMS = -1;
+	private int m_hangTimeMS = -1;
+	private boolean m_allFade = false;
+	private int m_loop = -1;
+	private int m_partCount = -1;
+	private String m_notes = "";
+	private String m_scene = "";
+	private boolean m_sceneEnd = false;
+	private int m_partIndex = -1;
 
 	/**
 	 * Get the information for a cue list from the EOS server.
@@ -69,7 +108,9 @@ public class EOSCueInfo implements Comparable<EOSCueInfo>
 		final ArrayBlockingQueue<OSCMessage> replies = new ArrayBlockingQueue<>(10);
 		ReplyHandler replyHandler = oscConn.sendMessage(new OSCMessage(requestMethod), replyMethod, replies);
 		boolean gotListReply = false;
+		boolean gotFxReply = false;
 		boolean gotLinksReply = false;
+		boolean gotActionsReply = false;
 		while (true) {
 			try {
 				OSCMessage msg = replies.poll(timeoutMS, TimeUnit.MILLISECONDS);
@@ -81,7 +122,8 @@ public class EOSCueInfo implements Comparable<EOSCueInfo>
 							GET_CUE_INFO_REPLY_CUE_NUMBER,
 							GET_CUE_INFO_REPLY_PART_NUMBER,
 							GET_CUE_INFO_REPLY_TYPE);
-				if (cmdTokens.get(2).equals(GET_CUE_INFO_REPLY_TYPE_LIST)) {
+				String respType = cmdTokens.get(2);
+				if (respType.equals(GET_CUE_INFO_REPLY_TYPE_LIST)) {
 					int part;
 					try {
 						part = Integer.parseInt(cmdTokens.get(1));
@@ -95,12 +137,43 @@ public class EOSCueInfo implements Comparable<EOSCueInfo>
 					}
 					m_uuid = msg.getString(GET_CUE_INFO_REPLY_FLD_UUID, m_label);
 					m_label = msg.getString(GET_CUE_INFO_REPLY_FLD_LABEL, m_label);
+					m_upTimeMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_UP_TIME, m_upTimeMS);
+					m_upDelayMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_UP_DELAY, m_upDelayMS);
+					m_downTimeMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_DOWN_TIME, m_downTimeMS);
+					m_downDelayMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_DOWN_DELAY, m_downDelayMS);
+					m_focusTimeMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_FOCUS_TIME, m_focusTimeMS);
+					m_focusDelayMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_FOCUS_DELAY, m_focusDelayMS);
+					m_colorTimeMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_COLOR_TIME, m_colorTimeMS);
+					m_colorDelayMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_COLOR_DELAY, m_colorDelayMS);
+					m_beamTimeMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_BEAM_TIME, m_beamTimeMS);
+					m_beamDelayMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_BEAM_DELAY, m_beamDelayMS);
+					m_rate = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_RATE, m_rate);
+					m_mark = msg.getString(GET_CUE_INFO_REPLY_FLD_MARK, m_mark);
+					m_block = msg.getString(GET_CUE_INFO_REPLY_FLD_BLOCK, m_block);
+					m_assert = msg.getString(GET_CUE_INFO_REPLY_FLD_ASSERT, m_assert);
+					m_link = msg.getString(GET_CUE_INFO_REPLY_FLD_LINK, m_link);
+					m_followTimeMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_FOLLOW_TIME, m_followTimeMS);
+					m_hangTimeMS = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_HANG_TIME, m_hangTimeMS);
+					m_allFade = msg.getBoolean(GET_CUE_INFO_REPLY_FLD_ALL_FADE, m_allFade);
+					m_loop = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_LOOP, m_loop);
+					m_partCount = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_PART_COUNT, m_partCount);
+					m_notes = msg.getString(GET_CUE_INFO_REPLY_FLD_NOTES, m_notes);
+					m_scene = msg.getString(GET_CUE_INFO_REPLY_FLD_SCENE, m_scene);
+					m_sceneEnd = msg.getBoolean(GET_CUE_INFO_REPLY_FLD_SCENE_END, m_sceneEnd);
+					m_partIndex = (int)msg.getLong(GET_CUE_INFO_REPLY_FLD_PART_INDEX, m_partIndex);
 					gotListReply = true;
-				} else if (cmdTokens.get(1).equals(GET_CUE_INFO_REPLY_TYPE_LINKS)) {
+				} else if (respType.equals(GET_CUE_INFO_REPLY_TYPE_LINKS)) {
+					// XXX ???
 					gotLinksReply = true;
+				} else if (respType.equals(GET_CUE_INFO_REPLY_TYPE_FX)) {
+					// XXX ???
+					gotFxReply = true;
+				} else if (respType.equals(GET_CUE_INFO_REPLY_TYPE_ACTIONS)) {
+					// XXX ???
+					gotActionsReply = true;
 				}
-				if (gotListReply && gotLinksReply) {
-					// System.out.println("XXX: got Cuelist " + m_cuelistNumber);
+				if (gotListReply && gotLinksReply && gotFxReply && gotActionsReply) {
+					// System.out.println("XXX: got Cue " + m_cuetNumber.toFullString());
 					oscConn.dropReplyHandler(replyHandler);
 					break;
 				}
